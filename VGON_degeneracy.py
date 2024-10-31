ba93ce7e1689a570346b3f4ff479df9fa9c59a3e
GhostArtyom <ghostartyomns@gmail.com>
2024-11-01 01:16:54 +0800

    Feature: class VAE_Model as a single .py file

--- VGON_degeneracy.py
+++ VGON_degeneracy.py
@@ -2,13 +2,12 @@ import time
 import torch
 import GPUtil
 import numpy as np
-import torch.nn as nn
 import pennylane as qml
 from typing import List
 from logging import info
 from logger import Logger
 from scipy.io import savemat
-import torch.nn.functional as F
+from VAE_model import VAEModel
 from itertools import combinations
 import torch.distributions as dists
 from torch.utils.data import DataLoader
@@ -106,50 +105,8 @@ def circuit_expval(n_layers: int, params: torch.Tensor, Ham):
     return qml.expval(Ham)
 
 
-class VAE_Model(nn.Module):
-
-    def __init__(self, n_params: int, z_dim: int, h_dim: List[int]):
-        super(VAE_Model, self).__init__()
-        # encoder
-        self.e1 = nn.ModuleList([nn.Linear(n_params, h_dim[0], bias=True)])
-        self.e1 += [nn.Linear(h_dim[i - 1], h_dim[i], bias=True) for i in range(1, len(h_dim))]
-        self.e2 = nn.Linear(h_dim[-1], z_dim, bias=True)
-        self.e3 = nn.Linear(h_dim[-1], z_dim, bias=True)
-        # decoder
-        self.d4 = nn.ModuleList([nn.Linear(z_dim, h_dim[-1], bias=True)])
-        self.d4 += [nn.Linear(h_dim[-i + 1], h_dim[-i], bias=True) for i in range(2, len(h_dim) + 1)]
-        self.d5 = nn.Linear(h_dim[0], n_params, bias=True)
-
-    def encoder(self, x):
-        h = F.relu(self.e1[0](x))
-        for i in range(1, len(self.e1)):
-            h = F.relu(self.e1[i](h))
-        mean = self.e2(h)
-        log_var = self.e3(h)
-        return mean, log_var
-
-    def reparameterize(self, mean, log_var):
-        eps = torch.randn(log_var.shape).to(device)
-        std = torch.exp(log_var).pow(0.5)
-        z = mean + std * eps
-        return z
-
-    def decoder(self, z):
-        params = F.relu(self.d4[0](z))
-        for i in range(1, len(self.d4)):
-            params = F.relu(self.d4[i](params))
-        params = self.d5(params)
-        return params
-
-    def forward(self, x):
-        mean, log_var = self.encoder(x)
-        z = self.reparameterize(mean, log_var)
-        params = self.decoder(z)
-        return params, mean, log_var
-
-
 Ham = Hamiltonian(n_qudits, beta)
-model = VAE_Model(n_params, z_dim, h_dim).to(device)
+model = VAEModel(n_params, z_dim, h_dim).to(device)
 if checkpoint:
     state_dict = torch.load(f'./mats/{checkpoint}.pt', map_location=device, weights_only=True)
     model.load_state_dict(state_dict)
--- VGON_generating.py
+++ VGON_generating.py
@@ -4,13 +4,12 @@ import time
 import torch
 import GPUtil
 import numpy as np
-import torch.nn as nn
 import pennylane as qml
 from typing import List
 from logging import info
 from logger import Logger
 from scipy.io import loadmat
-import torch.nn.functional as F
+from VAE_model import VAEModel
 from itertools import combinations
 import torch.distributions as dists
 from numpy.linalg import matrix_rank
@@ -73,49 +72,10 @@ def testing(batch_size: int, n_test: int, energy_upper: float):
         qml.layer(qutrit_symmetric_ansatz, n_layers, params)
         return qml.expval(Ham)
 
-    class VAE_Model(nn.Module):
 
-        def __init__(self, n_params: int, z_dim: int, h_dim: List[int]):
-            super(VAE_Model, self).__init__()
-            # encoder
-            self.e1 = nn.ModuleList([nn.Linear(n_params, h_dim[0], bias=True)])
-            self.e1 += [nn.Linear(h_dim[i - 1], h_dim[i], bias=True) for i in range(1, len(h_dim))]
-            self.e2 = nn.Linear(h_dim[-1], z_dim, bias=True)
-            self.e3 = nn.Linear(h_dim[-1], z_dim, bias=True)
-            # decoder
-            self.d4 = nn.ModuleList([nn.Linear(z_dim, h_dim[-1], bias=True)])
-            self.d4 += [nn.Linear(h_dim[-i + 1], h_dim[-i], bias=True) for i in range(2, len(h_dim) + 1)]
-            self.d5 = nn.Linear(h_dim[0], n_params, bias=True)
-
-        def encoder(self, x):
-            h = F.relu(self.e1[0](x))
-            for i in range(1, len(self.e1)):
-                h = F.relu(self.e1[i](h))
-            mean = self.e2(h)
-            log_var = self.e3(h)
-            return mean, log_var
-
-        def reparameterize(self, mean, log_var):
-            eps = torch.randn(log_var.shape).to(device)
-            std = torch.exp(log_var).pow(0.5)
-            z = mean + std * eps
-            return z
-
-        def decoder(self, z):
-            params = F.relu(self.d4[0](z))
-            for i in range(1, len(self.d4)):
-                params = F.relu(self.d4[i](params))
-            params = self.d5(params)
-            return params
-
-        def forward(self, x):
-            mean, log_var = self.encoder(x)
-            z = self.reparameterize(mean, log_var)
-            params = self.decoder(z)
-            return params, mean, log_var
 
     state_dict = torch.load(f'{path}.pt', map_location=device, weights_only=True)
-    model = VAE_Model(n_params, z_dim, h_dim).to(device)
+    model = VAEModel(n_params, z_dim, h_dim).to(device)
     model.load_state_dict(state_dict)
     model.eval()
 
